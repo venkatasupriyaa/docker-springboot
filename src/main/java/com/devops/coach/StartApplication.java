e39a3dce6fdb9435ab2b3b21e5674018e806b4ae
t-manish-kumar <141441980+t-manish-kumar@users.noreply.github.com>
2024-12-23 23:02:34 +0530

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -3,7 +3,7 @@ package com.devops.coach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springfra mework.ui.Model; 
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
